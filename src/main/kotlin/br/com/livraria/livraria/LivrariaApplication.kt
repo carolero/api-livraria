62408a6f8c823abc8c76ee6b4e7e21e3e24b2245
carolero <caroleudoxia@gmail.com>
2022-10-23 12:49:02 -0300

    Primeiro commit

---
+++ src/main/kotlin/br/com/livraria/livraria/LivrariaApplication.kt
@@ -0,0 +1,11 @@
+package br.com.livraria.livraria
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LivrariaApplication
+
+fun main(args: Array<String>) {
+	runApplication<LivrariaApplication>(*args)
+}
